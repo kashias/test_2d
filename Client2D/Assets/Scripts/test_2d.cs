374bfa3fccfb2ebea282c0de5fb9b379579399be
kashias <kashias@naver.com>
2019-08-27 00:07:20 +0900

    7

--- Client2D/Assets/Scripts/test_2d.cs
+++ Client2D/Assets/Scripts/test_2d.cs
@@ -6,7 +6,7 @@ public class test_2d : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int i_test = 3;	
+		int i_test = 7;	
 	}	
 	// Update is called once per frame
 	void Update () {
